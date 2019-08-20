80e8292041a9d5974f1cb4f6d42c68a5e462e16a
Oluwatola Oluwafemi Blessing <53811965+Oluwa-Femi@users.noreply.github.com>
2019-08-20 14:31:52 +0100

    Array challenge 3 done

--- assignments/array-methods.js
+++ assignments/array-methods.js
@@ -74,6 +74,7 @@ console.log(firstNamesAllCaps);
 // ==== Challenge 3: Use .filter() ====
 // The large shirts won't be available for the event due to an ordering issue. We need a filtered version of the runners array, containing only those runners with large sized shirts so they can choose a different size. This will be an array of objects.
 let runnersLargeSizeShirt = [];
+runners.filter(runner => {if (runner.shirt_size === 'L') {runnersLargeSizeShirt.push(runner)}});
 console.log(runnersLargeSizeShirt);
 
 // ==== Challenge 4: Use .reduce() ====
